563104da27145b7e0e7a0cbb84668987baafb0ef
kodama <jun.kodama@nikon.com>
2022-11-15 10:13:59 +0900

    masterとfor_understandで処理結果に違いが生じないか確認するための修 正

---
+++ ch05/check.sh
@@ -0,0 +1,5 @@
+git checkout master
+python train_neuralnet.py 1000
+
+git checkout for_understand
+python train_neuralnet.py 1000
--- ch05/train_neuralnet.py
+++ ch05/train_neuralnet.py
@@ -11,7 +11,11 @@ from two_layer_net import TwoLayerNet
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
-iters_num = 10000
+
+if sys.argv[1] is not None:
+    iters_num = int(sys.argv[1])
+else:
+    iters_num = 10000
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
@@ -26,18 +30,18 @@ for i in range(iters_num):
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
-    
+
     # 勾配
     #grad = network.numerical_gradient(x_batch, t_batch)
     grad = network.gradient(x_batch, t_batch)
-    
+
     # 更新
     for key in ('W1', 'b1', 'W2', 'b2'):
         network.params[key] -= learning_rate * grad[key]
-    
+
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
-    
+
     if i % iter_per_epoch == 0:
         train_acc = network.accuracy(x_train, t_train)
         test_acc = network.accuracy(x_test, t_test)
