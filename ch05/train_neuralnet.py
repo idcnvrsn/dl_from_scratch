a0f9039a1d42bfc125b7ea4b1bd127710d998b51
kodama <jun.kodama@nikon.com>
2022-11-15 15:36:50 +0900

    random seed設定

--- ch05/train_neuralnet.py
+++ ch05/train_neuralnet.py
@@ -6,16 +6,15 @@ import numpy as np
 from dataset.mnist import load_mnist
 from two_layer_net import TwoLayerNet
 
+import numpy as np
+np.random.seed(1)
+
 # データの読み込み
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
-
-if sys.argv[1] is not None:
-    iters_num = int(sys.argv[1])
-else:
-    iters_num = 10000
+iters_num = 10000
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
